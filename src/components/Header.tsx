20ce1df867297be1f75f6a4b766f569bf0e35807
TVV <TVV@d.c>
2023-08-24 00:33:25 +0300

    NavigationMenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,38 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Col, Container, Form, Navbar, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../images/logo.png";
 import TopMenu from "./TopMenu";
 
 const Header: React.FC = () => {
   return (
-    <Navbar bg="light">
+    <Navbar bg="light" className="shadow z-1">
       <Container>
-        <Navbar.Brand
-          className="d-flex align-items-center text-uppercase text-success fw-bold"
-          href="./"
-        >
-          <img
-            alt=""
-            src={logo}
-            width="50"
-            height="50"
-            className="d-inline-block align-top me-2"
-          />
-          Inventory
-        </Navbar.Brand>
+        <Row className="w-100 align-items-center">
+          <Col xs={4} lg={3}>
+            <Navbar.Brand
+              className="d-flex align-items-center text-uppercase text-success fw-bold"
+              href="./"
+            >
+              <img
+                alt=""
+                src={logo}
+                width="50"
+                height="50"
+                className="d-inline-block align-top me-2"
+              />
+              Inventory
+            </Navbar.Brand>
+          </Col>
 
-        <div className="ml-auto">
-          <TopMenu />
-        </div>
+          <Col xs={6} lg={3}>
+            <Form.Control type="text" placeholder="Поиск" className="h-50" />
+          </Col>
+
+          <Col xs={2} lg={6}>
+            <TopMenu />
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React from "react";
+import avatar from "../images/avatar.png";
+import { Container, Nav, Navbar, Image } from "react-bootstrap";
+import { GearFill } from "react-bootstrap-icons";
 
 const NavigationMenu: React.FC = () => {
   return (
-    <div>Navigation Menu</div>
+    <Navbar
+      bg="light"
+      className="position-fixed left-0 shadow align-items-start"
+      style={{ height: "100vh", width: 200, left: 0, top: 0, marginTop: 124 }}
+    >
+      <Container className="d-flex flex-column">
+        <div className="position-relative mb-5">
+          <Image
+            width={100}
+            height={100}
+            alt="avatar"
+            src={avatar}
+            roundedCircle
+          />
+          <div
+            className="position-absolute d-flex justify-content-center align-items-center bottom-0 end-0 shadow"
+            style={{
+              width: 36,
+              height: 36,
+              background: "white",
+              borderRadius: "50%",
+            }}
+          >
+            <GearFill width={14} height={14} fill="grey" />
+          </div>
+        </div>
+
+        <Nav className="d-flex flex-column align-items-center text-uppercase fw-bold">
+          <Nav.Link href="/home" className="nav-link">
+            Приход
+          </Nav.Link>
+          <Nav.Link href="/groups" className="nav-link">
+            Группы
+          </Nav.Link>
+          <Nav.Link href="/products" className="nav-link">
+            Продукты
+          </Nav.Link>
+          <Nav.Link href="/users" className="nav-link">
+            Пользователи
+          </Nav.Link>
+          <Nav.Link href="/settings" className="nav-link">
+            Настройки
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
-}
+};
 
 export default NavigationMenu;
