c1988346036040ce30e1207e0335d3e8578accda
TVV <TVV@d.c>
2023-08-24 14:58:14 +0300

    date and active session client logic

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import io from "socket.io-client";
 
 const TopMenu: React.FC = () => {
-  return <div className="d-flex justify-content-end">Top Menu</div>;
+  const [dateTime, setDateTime] = React.useState(new Date());
+  const [activeSessions, setActiveSessions] = React.useState(0);
+
+  React.useEffect(() => {
+    const socket = io("http://localhost:3001");
+
+    socket.on("activeSessionsUpdate", (count: number) => {
+      setActiveSessions(count);
+    });
+
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => {
+      socket.disconnect();
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div className="d-flex flex-column align-items-end">
+      <div className="date-time">
+        {dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}
+      </div>
+      <div className="active-sessions">Активные сессии: {activeSessions}</div>
+    </div>
+  );
 };
 
 export default TopMenu;
